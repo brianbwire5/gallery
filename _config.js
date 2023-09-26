a6add40f7cde7575b39c44e0a76ff549ec1d9794
Brayo <brianbwire5>
2023-09-26 16:44:17 +0300

    Fixed env variables

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://brianbwire:<password>@gallery.leelxxf.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://brianbwire:<password>@gallery.leelxxf.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://brianbwire:<password>@gallery.leelxxf.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://brianbwire:Brayo@2788@gallery.leelxxf.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://brianbwire:Brayo@2788@gallery.leelxxf.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://brianbwire:Brayo@2788@gallery.leelxxf.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
