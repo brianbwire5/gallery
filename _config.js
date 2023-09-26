9961abfbfe2bc631e3c2481fd075ec4b4ec2bfb8
Brayo <brianbwire5>
2023-09-26 16:33:52 +0300

    Changed mongodb creds

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://brianbwire:Brayo2788@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://brianbwire:Brayo2788@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://btianbwire:Brayo2788@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://brianbwire:<password>@gallery.leelxxf.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://brianbwire:<password>@gallery.leelxxf.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://brianbwire:<password>@gallery.leelxxf.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
